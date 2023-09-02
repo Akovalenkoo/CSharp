dff3184c008b1bbb0e26fcc852653f7a9bd72e79
SashaKovalenk0 <143830421+SashaKovalenk0@users.noreply.github.com>
2023-09-02 16:29:09 +0300

    Add homework

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int a = 1;
+int b = 2;
+int c = 3;
+Console.WriteLine($"{c}{b}{a}");
+
+
+// Написати програму, яка обчислює квадрат будь-якого введеного числа.
+
+/* int input = Convert.ToInt32(Console.ReadLine());
+int result = input * input;
+Console.WriteLine(result);
+Console.ReadKey(); */
+
+//Дано тризначне число. Знайти число, отримане під час перестановки першої та другої цифр заданого числа.
+
+/* int a = 1;
+int b = 2;
+int c = 3;
+Console.WriteLine($"{a}{b}{c}");
+Console.WriteLine($"{b}{a}{c}"); */
+
+// Написати програму, яка обчислює середнє арифметичне двох чисел.
+/* int input = Convert.ToInt32(Console.ReadLine());
+int input2 = Convert.ToInt32(Console.ReadLine());
+int result = input * input2 / 2;
+Console.WriteLine(result);
+Console.ReadKey();*/
+
+//Дослідити інші способи поміняти місцями значення двох змінних без використання тимчасової змінної. 
+
+//byte a = 10; 
+//byte b = 11;
+//a = (byte)(b + a);
+//b = (byte)(a - b);
+//a = (byte)(a - b);
+//Console.WriteLine(a);
+//Console.WriteLine(b);
