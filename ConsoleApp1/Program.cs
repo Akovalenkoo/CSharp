f1dff6fe879b547cc0d88057e72c7d7acff38a8c
SashaKovalenk0 <143830421+SashaKovalenk0@users.noreply.github.com>
2023-09-04 08:39:28 +0300

    Revision of homework

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,38 +1,83 @@
-﻿
-int a = 1;
-int b = 2;
-int c = 3;
-Console.WriteLine($"{c}{b}{a}");
+﻿// Дано тризначне число. Знайти число, отримане під час прочитання його цифр справа наліво.
+
+//Console.Write("Введіть тризначне число: ");
+//string input = Console.ReadLine();
+
+//// Перетворення рядка у число
+//int number = int.Parse(input);
+
+//// Отримання окремих цифр
+//int digit1 = number % 10;
+//int digit2 = (number / 10) % 10;
+//int digit3 = (number / 100) % 10;
+
+//// Вивід цифр у зворотньому порядку
+//Console.WriteLine($"{ digit1}{digit2}{digit3}");
+
+//// Очікування натискання клавіші перед завершенням програми
+//Console.ReadKey();
 
 
 // Написати програму, яка обчислює квадрат будь-якого введеного числа.
 
-/* int input = Convert.ToInt32(Console.ReadLine());
-int result = input * input;
-Console.WriteLine(result);
-Console.ReadKey(); */
+//int input = Convert.ToInt32(Console.ReadLine());
+//int result = input * input;
+//Console.WriteLine(result);
+//Console.ReadKey();
 
 //Дано тризначне число. Знайти число, отримане під час перестановки першої та другої цифр заданого числа.
 
-/* int a = 1;
-int b = 2;
-int c = 3;
-Console.WriteLine($"{a}{b}{c}");
-Console.WriteLine($"{b}{a}{c}"); */
+//Console.Write("Введіть тризначне число: ");
+//string input = Console.ReadLine();
+
+//// Перетворення рядка у число
+//int number = int.Parse(input);
+
+//// Отримання окремих цифр
+//int digit1 = number % 10;
+//int digit2 = (number / 10) % 10;
+//int digit3 = (number / 100) % 10;
+
+//// Вивід цифр у зворотньому порядку
+//Console.WriteLine($"{digit2}{digit3}{digit1}");
+
+//// Очікування натискання клавіші перед завершенням програми
+//Console.ReadKey();
 
 // Написати програму, яка обчислює середнє арифметичне двох чисел.
-/* int input = Convert.ToInt32(Console.ReadLine());
-int input2 = Convert.ToInt32(Console.ReadLine());
-int result = input * input2 / 2;
-Console.WriteLine(result);
-Console.ReadKey();*/
+
+
+//int input = Convert.ToInt32(Console.ReadLine());
+//int input2 = Convert.ToInt32(Console.ReadLine());
+//int result = input + input2 / 2;
+//Console.WriteLine(result);
+//Console.ReadKey();
+
+//АБО
+
+
+//Console.Write("Введіть перше число: ");
+//double num1 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("Введіть друге число: ");
+//double num2 = Convert.ToDouble(Console.ReadLine());
+
+//// Обчислення середнього арифметичного
+//double середнє_арифметичне = (num1 + num2) / 2;
+
+//Console.WriteLine("Середнє арифметичне чисел {0} і {1} дорівнює {2}", число1, число2, середнє_арифметичне);
+
+//// Очікування натискання клавіші перед завершенням програми
+//Console.ReadKey();
 
 //Дослідити інші способи поміняти місцями значення двох змінних без використання тимчасової змінної. 
 
-//byte a = 10; 
-//byte b = 11;
-//a = (byte)(b + a);
-//b = (byte)(a - b);
-//a = (byte)(a - b);
-//Console.WriteLine(a);
-//Console.WriteLine(b);
+//int a = 5;
+//int b = 10;
+
+//a = a ^ b;
+//b = a ^ b;
+//a = a ^ b;
+
+//Console.WriteLine("a = " + a);
+//Console.WriteLine("b = " + b);
