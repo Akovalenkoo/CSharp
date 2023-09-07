7041a8665b98f75def2ed8c310fb4ba60b8c5bc3
SashaKovalenk0 <143830421+SashaKovalenk0@users.noreply.github.com>
2023-09-07 09:48:27 +0300

    Add FirsProject

---
+++ C#/FirstProj/Program.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+
+//namespace HelloWorld
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Напищіт Число");
+//            int number = int.Parse(Console.ReadLine());
+
+//            if (number % 2 == 0)
+//            {
+//                Console.WriteLine("Парне");
+//                Console.ReadKey();
+//            }
+//            else
+//            {
+//                Console.WriteLine("Не парне");
+//                Console.ReadKey();
+//            }
+
+//        }
+//    }
+//}
---
+++ C#/FirstProj/Program2.cs
@@ -0,0 +1,26 @@
+﻿//class Program
+//{
+//    static void Main()
+//    {
+//        Console.Write("Введіть перше число: ");
+//        int number = int.Parse(Console.ReadLine());
+
+//        Console.Write("Введіть друге число: ");
+//        int number2 = int.Parse(Console.ReadLine());
+
+//        if (number == number2)
+//        {
+//            Console.WriteLine($"{number} = {number2}");
+//            Console.ReadKey();
+//        } else if (number > number2)
+//        {
+//            Console.WriteLine($"{number} > {number2}");
+//            Console.ReadKey();
+//        } else
+//        {
+//            Console.WriteLine($"{number} < {number2}");
+//            Console.ReadKey();
+//        }
+
+//    }
+//}
