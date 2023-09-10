f001b294dcede6ce9235d2047d90f5c65c5df180
SashaKovalenk0 <143830421+SashaKovalenk0@users.noreply.github.com>
2023-09-10 19:29:13 +0300

    йццй

--- C#/Lesson_4/HW_4_4/Program.cs
+++ C#/Lesson_4/HW_4_4/Program.cs
@@ -1,39 +1,39 @@
-﻿//using System;
+﻿using System;
 
-//class Program
-//{
-//    static bool IsPrime(int number)
-//    {
-//        if (number <= 1)
-//        {
-//            return false; 
-//        }
+class Program
+{
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
 
-//        for (int i = 2; i * i <= number; i++)
-//        {
-//            if (number % i == 0)
-//            {
-//                return false; 
-//            }
-//        }
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
 
-//        return true; 
-//    }
+        return true;
+    }
 
-//    static void Main()
-//    {
-//        Random random = new Random();
-//        int genNum = random.Next(2, 1000);
+    static void Main()
+    {
+        Random random = new Random();
+        int genNum = random.Next(2, 1000);
 
-//        if (IsPrime(genNum))
-//        {
-//            Console.WriteLine($"{genNum} is prime.");
-//        }
-//        else
-//        {
-//            Console.WriteLine($"{genNum} is not prime.");
-//        }
-//        Console.ReadKey();
-//    }
-//}
+        if (IsPrime(genNum))
+        {
+            Console.WriteLine($"{genNum} is prime.");
+        }
+        else
+        {
+            Console.WriteLine($"{genNum} is not prime.");
+        }
+        Console.ReadKey();
+    }
+}
 
