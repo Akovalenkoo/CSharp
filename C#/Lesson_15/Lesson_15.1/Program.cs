67d9187907e59401977c59ff6c6d60a0aa38351b
SashaKovalenk0 <143830421+SashaKovalenk0@users.noreply.github.com>
2023-10-22 17:34:13 +0300

    Add Lesson_15

---
+++ C#/Lesson_15/Lesson_15.1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // 1) Фільтрація
+        List<int> numbers = new List<int> { 2, 5, 8, 12, 15, 18, 22 };
+        var filteredNumbers = numbers.Where(n => n > 10);
+        Console.WriteLine("Фільтрація:");
+        Console.WriteLine(string.Join(", ", filteredNumbers));
+
+        // 2) Сортування
+        List<string> fruits = new List<string> { "Яблуко", "Апельсин", "Банан", "Ківі" };
+        var sortedFruits = fruits.OrderBy(fruit => fruit);
+        Console.WriteLine("\nСортування:");
+        Console.WriteLine(string.Join(", ", sortedFruits));
+
+        // 3) Видалення дублікатів
+        List<string> strings = new List<string> { "А", "Б", "В", "А", "Г", "В" };
+        var uniqueStrings = strings.Distinct();
+        Console.WriteLine("\nВидалення дублікатів:");
+        Console.WriteLine(string.Join(", ", uniqueStrings));
+
+        // 4) Підрахунок
+        List<int> grades = new List<int> { 85, 92, 78, 95, 88, 90 };
+        int studentsAbove90 = grades.Count(grade => grade > 90);
+        Console.WriteLine("\nПідрахунок студентів з оцінками більше 90 балів:");
+        Console.WriteLine(studentsAbove90);
+
+        // 5) Фільтрація та сортування
+        List<Product> products = new List<Product>
+        {
+            new Product { Name = "Продукт 1", Price = 45 },
+            new Product { Name = "Продукт 2", Price = 30 },
+            new Product { Name = "Продукт 3", Price = 60 },
+            new Product { Name = "Продукт 4", Price = 20 },
+        };
+        var cheapProducts = products.Where(product => product.Price < 50).OrderBy(product => product.Price);
+        Console.WriteLine("\nФільтрація та сортування товарів за ціною:");
+        foreach (var product in cheapProducts)
+        {
+            Console.WriteLine($"{product.Name} - {product.Price} грн");
+        }
+
+        // 6) Пошук максимального за індексом
+        List<int> integers = new List<int> { 10, 25, 8, 45, 15, 30, 55, 5 };
+        int maxByIndex = integers.Select((value, index) => new { Value = value, Index = index }).Max(item => item.Value);
+        Console.WriteLine("\nПошук максимального числа за індексом:");
+        Console.WriteLine($"Максимальне число: {maxByIndex}");
+        Console.ReadLine();
+    } 
+}
+
+class Product
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+}
+
---
+++ C#/Lesson_15/Lesson_15.2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>
+        {
+            new Student { Name = "Олена", Grade = 95, Course = 1 },
+            new Student { Name = "Іван", Grade = 88, Course = 2 },
+            new Student { Name = "Марія", Grade = 92, Course = 1 },
+            new Student { Name = "Петро", Grade = 85, Course = 2 },
+            new Student { Name = "Анна", Grade = 90, Course = 3 },
+            new Student { Name = "Максим", Grade = 89, Course = 3 }
+        };
+
+        var highScoringStudents = students.Where(student => student.Grade >= 90);
+        Console.WriteLine("Фільтрація (студенти з оцінкою 90 і вище):");
+        foreach (var student in highScoringStudents)
+        {
+            Console.WriteLine($"Ім'я: {student.Name}, Оцінка: {student.Grade}");
+        }
+
+        var sortedStudents = students.OrderByDescending(student => student.Grade);
+        Console.WriteLine("\nСортування (студенти за оцінкою в спадаючому порядку):");
+        foreach (var student in sortedStudents)
+        {
+            Console.WriteLine($"Ім'я: {student.Name}, Оцінка: {student.Grade}");
+        }
+
+        var groupedByCourse = students.GroupBy(student => student.Course);
+        Console.WriteLine("\nГрупування студентів за курсами:");
+        foreach (var group in groupedByCourse)
+        {
+            Console.WriteLine($"Курс {group.Key}:");
+            foreach (var student in group)
+            {
+                Console.WriteLine($"Ім'я: {student.Name}, Оцінка: {student.Grade}");
+            }
+        }
+
+        var countByCourse = students.GroupBy(student => student.Course)
+                                    .Select(group => new { Course = group.Key, Count = group.Count() });
+        Console.WriteLine("\nПідрахунок студентів на кожному курсі:");
+        foreach (var item in countByCourse)
+        {
+            Console.WriteLine($"Курс {item.Course}: {item.Count} студентів");
+        }
+
+        var projection = students.Select(student => $"Ім'я: {student.Name}, Оцінка: {student.Grade}");
+        Console.WriteLine("\nПроекція (рядки з ім'ям та оцінкою кожного студента):");
+        foreach (var item in projection)
+        {
+            Console.WriteLine(item);
+            Console.ReadLine();
+        }
+    }
+}
+
+class Student
+{
+    public string Name { get; set; }
+    public int Grade { get; set; }
+    public int Course { get; set; }
+}
+
